1db8d9fa7c3c4a4dec4f36a490a5e2aeb9501875
Michael Ludwig <mluds@ymail.com>
2014-11-27 03:08:29 -0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: 'example@gmail.com', name: "Example User", password: 'password', password_confirmation: 'password', confirmed_at: Time.now)
+User.create(id: 1, email: 'example@gmail.com', name: "Example User", password: 'password', password_confirmation: 'password', confirmed_at: Time.now)
 AuthorableProblem.create(user_id: 1, problem_text: "Aliquam erat volutpat. Pellentesque in tincidunt nunc. Suspendisse luctus posuere quam sit amet ultricies. Praesent feugiat quam volutpat dui placerat, sed tempor enim consequat. Morbi risus nulla, lobortis id porta.")
 AuthorableProblem.create(user_id: 1, problem_text: "Curabitur scelerisque mauris nec interdum ullamcorper. In dictum orci vitae tortor tincidunt blandit. Sed pharetra nisl mauris, vel consequat enim semper in. Morbi vestibulum augue vel lacinia cursus. In hac.")
 AuthorableProblem.create(user_id: 1, problem_text: "Quisque accumsan eros ac turpis pharetra, in accumsan magna vestibulum. Aliquam accumsan, lorem quis aliquet posuere, tellus dolor euismod augue, sit amet bibendum sem purus eu libero. Sed lacinia eu.")
