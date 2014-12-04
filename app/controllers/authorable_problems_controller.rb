bc2b6a0939b0910f0ce6677f5d2f303eb3b799f6
Michael Ludwig <mluds@ymail.com>
2014-12-04 02:26:52 -0500

    Changed notification styling

--- app/controllers/authorable_problems_controller.rb
+++ app/controllers/authorable_problems_controller.rb
@@ -6,14 +6,14 @@ class AuthorableProblemsController < ApplicationController
   def view
     @auth_problems = AuthorableProblem.all
   end
-  
+
   def new
     @auth_problem = AuthorableProblem.new
     @submit_url = create_auth_problem_path
     @submit_text = "Create"
     render :edit
   end
-  
+
   def create
     auth_problem = AuthorableProblem.new
     auth_problem.user = current_user
@@ -26,30 +26,30 @@ class AuthorableProblemsController < ApplicationController
       redirect_to new_auth_problem_path
     end
   end
-  
+
   def edit
     @auth_problem = AuthorableProblem.find(params[:id])
     @submit_url = update_problem_path
     @submit_text = "Update"
   end
-  
+
   def update
     auth_problem = AuthorableProblem.find(params[:id])
     auth_problem.problem_text = params[:authorable_problem][:problem_text]
     if auth_problem.save
       flash[:notice] = "Problem saved"
     else
-      flash[:alert] = auth_problem.errors
+      flash[:alert] = auth_problem.errors.full_messages.first
     end
     redirect_to edit_problem_path
   end
-  
+
   def delete
     auth_problem = AuthorableProblem.find(params[:id])
     if auth_problem.destroy
       flash[:notice] = "Problem deleted"
     else
-      flash[:alert] = auth_problem.errors
+      flash[:alert] = auth_problem.errors.full_messages.first
     end
     redirect_to :back
   end
