053605e2ca32193148faf1536a39bf91069c61b8
Michael Ludwig <mluds@ymail.com>
2014-12-07 05:24:09 -0500

    Small touchups

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -2,8 +2,7 @@ module AssignmentsHelper
   def score(assign)
     correct = assign.problems.where(completed: true).size
     total = assign.problems.size
-    percent = Float(correct) / total * 100
 
-    correct.to_s + " / " + total.to_s + " (" + percent.to_s + "%)"
+    correct.to_s + " / " + total.to_s
   end
 end
